dc6123088839cc8f11a5bbc6fa80c2e12443149d
Toast3y <toast3y@gmail.com>
2015-12-11 01:58:02 +0000

    Added Rainbow Chaser Model, script.

---
+++ Rainbow Chaser/Assets/MasterCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterCube : MonoBehaviour {
+
+	
+
+
+	// Use this for initialization
+	void Start () {
+        //Generate position in game world according to player and set it
+        //Draw vectors in vectrosity according to position
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
