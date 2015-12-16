b037d98013ab4a0338571aa8a9d441f5a329e042
Toast3y <toast3y@gmail.com>
2015-12-16 00:18:58 +0000

    Added mouse movement, restraints

--- Rainbow Chaser/Assets/ControllerBehaviour.cs
+++ Rainbow Chaser/Assets/ControllerBehaviour.cs
@@ -3,30 +3,32 @@ using System.Collections;
 
 public class ControllerBehaviour : MonoBehaviour {
 
-	//Moves the attached "ship" gameObject using the scrollwheel, and controls speed.
-
-	//Determines starting game speed
-	public float speed = 5.0f;
+	//Moves the attached "ship" gameObject using the scrollwheel.
 	GameObject ship;
+	Animator animator;
 
-	
+	public float moveSpeed = 1.0f;
+
 
 	// Use this for initialization
 	void Start () {
 		//Script will act on only the object it is attached to, to allow reuse of code
 		ship = this.gameObject;
+
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		ship.transform.Translate(0, 0, speed * Time.deltaTime);
 
-		if (Input.GetAxis("Mouse ScrollWheel") > 0){
-			//ship.transform.Translate
+		if (Input.GetAxis("Mouse ScrollWheel") < 0 && ship.transform.position.x < 18.0f){
+			//Moves ship downwards
+			ship.transform.Translate(moveSpeed, 0 , 0);
 		}
-		else if (Input.GetAxis("Mouse ScrollWheel") < 0) {
-			//ship.transform.Translate
+		else if (Input.GetAxis("Mouse ScrollWheel") > 0 && ship.transform.position.x > -8.0f) {
+			//Moves ship upwards
+			ship.transform.Translate(-moveSpeed, 0, 0);
 		}
 	}
 }
