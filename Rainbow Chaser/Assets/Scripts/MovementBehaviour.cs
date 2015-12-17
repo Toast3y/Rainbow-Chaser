5cdf54f6daaf8002af2d0b6608452d4f1a8b4cd5
Toast3y <toast3y@gmail.com>
2015-12-17 04:15:10 +0000

    Added scaling difficulty

--- Rainbow Chaser/Assets/Scripts/MovementBehaviour.cs
+++ Rainbow Chaser/Assets/Scripts/MovementBehaviour.cs
@@ -5,13 +5,19 @@ public class MovementBehaviour : MonoBehaviour {
 
 	//Determines starting game speed
 	public float speed = 5.0f;
+	public float speedOT = 0.005f;
+	public float cubeFrequencyOT = 1.0f;
+	public int cubesMadeOT = 50;
 
 	//Determines frequency of cube spawns
-	public float cubeFrequency = 180.0f;
+	public float cubeFrequency = 360.0f;
 	public float upperBound = 17.0f;
 	public float lowerBound = -7.0f;
 	public GameObject ship;
 	public GameObject Cube;
+
+	private int cubesMade;
+	
 
 	// Use this for initialization
 	void Start () {
@@ -23,11 +29,25 @@ public class MovementBehaviour : MonoBehaviour {
 		//Move ship to the right according to game speed
 		transform.Translate(0, 0, speed * Time.deltaTime);
 
+		
+
 		//Check to see if another block should be made
 		var framesPassed = Time.frameCount % (cubeFrequency / speed);
 
-		//If it falls on zero / a certain range, spawn a cube
-		if (framesPassed == 0) {
+		//If it falls on zero / a certain range, spawn a cube and increase game speed
+		if (framesPassed >= 0.0f && framesPassed <= 1.0f) {
+			//Increase speed and spawn rate over time
+			//Add to speed for every block
+			speed = speed + speedOT;
+			cubesMade++;
+
+
+			//If enough cubes have spawned, reduce frequency
+			if (cubesMade % cubesMadeOT == 0 && cubeFrequency > 270.0f) {
+				cubeFrequency = cubeFrequency - cubeFrequencyOT;
+			}
+
+
 			GameObject newcube = GameObject.Instantiate(Cube);
 
 			Vector3 newPos = new Vector3(Random.Range(lowerBound, upperBound) ,5.0f,ship.transform.position.z + 60.0f);
