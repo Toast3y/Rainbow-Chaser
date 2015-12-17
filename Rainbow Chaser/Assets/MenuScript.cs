75908ab050e3e7c3d1f86233dc1bb349a30b5086
Toast3y <toast3y@gmail.com>
2015-12-17 18:00:58 +0000

    Added Main Menu

---
+++ Rainbow Chaser/Assets/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NewGame() {
+		SceneManager.LoadScene("scene1");
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
